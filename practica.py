10230a3d366b24b0d6cbf90eb27a9b141119dfaa
Dabv6799 <67011891+Dabv6799@users.noreply.github.com>
2021-03-15 21:18:07 -0600

    changes in practica.py

--- practica.py
+++ practica.py
@@ -1,29 +1,49 @@
-#import random
+import random
+import os
 
 
 class Historias:
-    personaje = ""
-    arma = ""
-    matar = ""
-    arreglo_pers = (["Luismi", "Chayanne", "Maribel Guardia", "Tom cruise", "Carmelita Salinas"])
+    personaje = 0
+    arma = 0
+    matar = 0
+    arreglo_pers = ['Luismi', 'Chayanne', 'Maribel Guardia', 'Tom cruise', 'Carmelita Salinas']
 
     def ventana_ini(self):
         print("Elige a quien matar")
-        matar = int(input())
+        for n in range(0, len(self.arreglo_pers)):
+            print(n+1, ".-",self.arreglo_pers[n])
+        self.matar = int(input())
+        self.arreglo_pers[n].pop(n)
+        os.system ("cls") 
+        
+        print("Se acaba de cometer un homicidio y tu eres el encargado de averiguar quien fue el asesino y como lo mataron")
+        print("Se te iran dando una serie de pistas para determinar el asesinato")
+        print("¡SUERTE EN EL DESAFIO!")
+        os.system ("cls") 
+        
+        if (self.matar == 1):
+            Clue.luismi()
+
+        if self.matar == 2:
+            pass
+        
+        if self.matar == 3:
+            pass
+        
+        if self.matar == 4:
+            pass
+        
+        if self.matar == 5:
+            pass
 
 
     def selec_nombre(self):
-        print("Selecciona el personaje que fue el culpable")
-        print("1.-Luismi")
-        print("2.-Chayanne")
-        print("3.-Maribel Guardia")
-        print("4.-Tom cruise")
-        print("5.-Carmelita Salinas")
-        self.personaje = int(input())
+        print("Selecciona el personaje al que quieres interrogar >:|")
         for n in range(0,len(self.arreglo_pers)):
-            if (n == self.personaje):
-                n=n-1
-                np.delete(self.arreglo_pers, n)
+            print(self.arreglo_pers[n])
+            #if n == :
+             #   print(n+1, ".-", self.arreglo_pers[n])
+            
 
 
     def select_arma(self):
@@ -35,16 +55,20 @@ class Historias:
         print("5.-Taladro")
         self.arma = int(input())
 
+
+    def historia1_luismi(self):
+        os.system ("cls") 
+        
     def luismi(self):
         print ("NARRATIVA DE COMO SUCEDIERON LOS HECHOS")
-        ale_luismi = random.randint(1,3)
-        if ale_luismi() == 1:
-            #def historia1_luismi():
-            pass
-        if ale_luismi() == 2:
+        #ale_luismi = random.randint(1,3)
+        ale_luismi = 1
+        if ale_luismi == 1:
+            self.selec_nombre()
+        if ale_luismi == 2:
             #def historia2_luismi():
             pass
-        if ale_luismi() == 3:
+        if ale_luismi == 3:
             #def historia3_luismi():
             pass
 
@@ -61,22 +85,6 @@ class Historias:
 Clue = Historias()
 Clue.ventana_ini()
 
-Clue.ventana_ini()
-print("Se acaba de cometer un homicidio y tu eres el encargado de averiguar quien fue el asesino y como lo mataron")
-print("Se te iran dando una serie de pistas para determinar el asesinato")
-print("¡SUERTE EN EL DESAFIO!")
 
-if Clue.matar == 1:
-    Clue.luismi()
 
-if Clue.matar == 2:
-    pass
 
-if Clue.matar == 3:
-    pass
-
-if Clue.matar == 4:
-    pass
-
-if Clue.matar == 5:
-    pass
